b590921952894f4d0799655983afe8f3dfd0f714
sugamasoushi <sugamasoushi@gmail.com>
2019-09-05 00:53:49 +0900

    ll

---
+++ src/main/java/com/heroherosite/HeroherositeApplication.java
@@ -0,0 +1,13 @@
+package com.heroherosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroherositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroherositeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/heroherosite/TestController.java
@@ -0,0 +1,17 @@
+package com.heroherosite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String index() {
+		return "herro world";
+	}
+
+}
