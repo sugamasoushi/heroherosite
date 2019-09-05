0c871e821084252253a61c8d526dbb4effdec216
sugamasoushi <sugamasoushi@gmail.com>
2019-09-05 11:48:56 +0900

    test

--- src/main/java/com/heroherosite/TestController.java
+++ src/main/java/com/heroherosite/TestController.java
@@ -2,13 +2,16 @@ package com.heroherosite;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class TestController {
-	
-	@RequestMapping("/")
-	public String index() {
+public class TestController{
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView mav) {
+    	
+    	System.out.println("はろー");
+    	mav.setViewName("index");
 
-		return  "index";
-	}
+        return mav;
+    }
 }
